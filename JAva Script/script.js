7a0ffd633266c113858ae7e10cc0ef0c9a5c0b3f
Pranav8077 <pranavmittal5002@gmail.com>
2025-09-17 10:00:14 +0530

    Initial commit with HTML file

---
+++ JAva Script/script.js
@@ -0,0 +1,53 @@
+//Output method
+// console.log("FSD class");
+// Input Method
+// let a=prompt("Enter a number:");
+//console.log=(a)
+
+// var a=20;
+// var a=30;  //can be redeclear 
+
+// let a=40;
+// console.log("After first assignment of b's Value" +b);
+// a=70;
+// console.log("After first assignment of b's Value" +b);
+// {
+//     let name ="Ravi"
+//     name="bhjk"
+//     console.log("name:" +name);
+// }
+
+// let firstName = "Pranav";
+// let lastName = "Mittal";
+// let message = "Hello";
+
+// let outputMessage = message + ", " + firstName + ", " + lastName;
+// console.log(outputMessage);
+
+// let sum=3 - 3 + "30";
+// console.log(sum)
+
+//typechecking
+// console.log(typeof("sum"));
+
+// Implicit type
+// String to number
+
+// console.log(a);
+// var a="30";
+// console.log(a);
+// {
+//     var a="50";
+//     console.log(a);
+//     console.log(message());
+//     function message(){
+//         console.log("Hello FSD class");
+//     }
+// }
+console.log("Hello");
+
+const student={
+    name:"Xyz",
+    rollno:123
+    email:
+}
